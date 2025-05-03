67da14f294f1787c876c2962250ee294f9b3eca8
Ryan Golhar <golharam@Ryans-iMac.local>
2025-05-02 22:46:01 -0400

    Remove /api/ url prefix

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -5,7 +5,7 @@ from flask import Blueprint
 from flask_restx import Api
 from .wes import api as wes_api
 
-BLUEPRINT_API = Blueprint('api', __name__, url_prefix='/api')
+BLUEPRINT_API = Blueprint('api', __name__, url_prefix='')
 API = Api(BLUEPRINT_API,
           title='GA4GH WES API',
           version='1.0',
