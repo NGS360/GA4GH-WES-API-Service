29655350ca4a76ed33d93ac63ff5c3bf5b604a43
Ryan Golhar <ryan.golhar@bms.com>
2025-12-15 13:09:31 -0500

    Move dependencies and remove old dir

--- core/deps.py
+++ core/deps.py
@@ -4,6 +4,10 @@ from sqlmodel import Session
 from fastapi import Depends
 
 
+#from src.wes_service.core.security import get_current_user
+#from src.wes_service.core.storage import StorageBackend, get_storage_backend
+#from src.wes_service.db.session import get_db
+
 def get_db() -> Generator[Session, None, None]:
     """Yield a database session."""
     from core.db import get_session
@@ -11,3 +15,5 @@ def get_db() -> Generator[Session, None, None]:
 
 
 DatabaseSession: TypeAlias = Annotated[Session, Depends(get_db)]
+# CurrentUser = Annotated[str, Depends(get_current_user)]
+# Storage = Annotated[StorageBackend, Depends(get_storage_backend)]
