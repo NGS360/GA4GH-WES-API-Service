49014e3b99d7fe7f6ccac4d6c533d8a7fb9d84aa
Ryan Golhar <ryan.golhar@bms.com>
2026-05-21 14:18:36 -0400

    Remove frontend code

--- wes-frontend/src/.next/static/chunks/d4b1c_modules_@tanstack_query-devtools_build_DevtoolsPanelComponent_PULY4AJ7_11dfe784.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_57afe0f2._.js"
-],
-    source: "dynamic"
-});
